8b711d2755cd5e335b2ceefa0cf17505e96280c8
Paw565pl <paw565pl@gmail.com>
2023-11-24 23:47:58 +0100

    converted seed endpoint to async one

--- app/routes/seed.py
+++ app/routes/seed.py
@@ -1,18 +1,19 @@
+import aiofiles
 from flask import jsonify, request
 from neomodel import clear_neo4j_database, db
 
 
-def seed_db():
+async def seed_db():
     data = request.get_json()
     seedAgree = data.get("seed")
 
     if not seedAgree and seedAgree != "yes":
         return jsonify({}), 400
 
-    with open("./app/data/seed_data.cypher", "r") as queryFile:
+    async with aiofiles.open("./app/data/seed_data.cypher", "r") as queryFile:
         clear_neo4j_database(db)
 
-        queryFileString = queryFile.read()
+        queryFileString = await queryFile.read()
         db.cypher_query(queryFileString)
 
         return jsonify({"message": "database seeded successfully"}), 201
