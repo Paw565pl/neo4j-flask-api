d62128ada8a8bae923338c307a460a31ed7b5015
Paw565pl <paw565pl@gmail.com>
2023-11-25 11:59:25 +0100

    added error handling to get employees endpoint

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from os import environ
 from dotenv import load_dotenv
 from flask import Flask
 from neomodel import config
-from routes.employees import employees
+from routes.employees import get_employees
 from routes.seed import seed_db
 
 load_dotenv()
@@ -12,7 +12,7 @@ config.DATABASE_URL = environ.get("NEO4J_BOLT_URL")
 app = Flask(__name__)
 
 app.add_url_rule("/seed", view_func=seed_db, methods=["POST"])
-app.add_url_rule("/employees", view_func=employees, methods=["GET"])
+app.add_url_rule("/employees", view_func=get_employees, methods=["GET"])
 
 if __name__ == "__main__":
     isDebugTurnedOn = bool(environ.get("DEBUG", False))
