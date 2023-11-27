0e648d189daee8f9725d986f0c175228af34ebf2
Paw565pl <paw565pl@gmail.com>
2023-11-27 22:08:15 +0100

    created command for seeding the db

--- app/app.py
+++ app/app.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 from flask import Flask
 from neomodel import config
 
+from app.commands import seed_db
 from app.routes.departments import departments_blueprint
 from app.routes.employees import employees_blueprint
-from app.routes.seed import seed_blueprint
 
 load_dotenv()
 config.DATABASE_URL = environ.get("NEO4J_BOLT_URL")
 
 app = Flask(__name__)
 
-app.register_blueprint(seed_blueprint, url_prefix="/seed")
 app.register_blueprint(employees_blueprint, url_prefix="/employees")
 app.register_blueprint(departments_blueprint, url_prefix="/departments")
+
+app.cli.add_command(seed_db)
---
+++ app/commands.py
@@ -0,0 +1,13 @@
+import click
+from neomodel import clear_neo4j_database, db
+
+
+@click.command(name="seed_db", help="Seed database with example data.")
+def seed_db():
+    print("seeding database...")
+    with open("./data/seed_data.cypher", "r") as queryFile:
+        clear_neo4j_database(db)
+
+        query_file_string = queryFile.read()
+        db.cypher_query(query_file_string)
+    print("done!")
