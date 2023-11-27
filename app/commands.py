199f9b32ab7bb10c96c32770b0cc533a122ee278
Paw565pl <paw565pl@gmail.com>
2023-11-27 23:05:38 +0100

    fixed location of data file

--- app/commands.py
+++ app/commands.py
@@ -5,7 +5,7 @@ from neomodel import clear_neo4j_database, db
 @click.command(name="seed_db", help="Seed database with example data.")
 def seed_db():
     print("seeding database...")
-    with open("./data/seed_data.cypher", "r") as queryFile:
+    with open("./app/data/seed_data.cypher", "r") as queryFile:
         clear_neo4j_database(db)
 
         query_file_string = queryFile.read()
