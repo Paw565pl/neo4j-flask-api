e01c87d1b6ded6806e2463d5159d85ba08265a06
Paw565pl <paw565pl@gmail.com>
2024-07-03 14:28:10 +0200

    refactor: delete comments and change sample data to english

--- app/commands.py
+++ app/commands.py
@@ -5,9 +5,9 @@ from neomodel import clear_neo4j_database, db
 @click.command(name="seed_db", help="Seed database with example data.")
 def seed_db():
     print("seeding database...")
-    with open("./app/data/seed_data.cypher", "r") as queryFile:
+    with open("./app/data/sample_data.cypher", "r") as f:
         clear_neo4j_database(db)
 
-        query_file_string = queryFile.read()
-        db.cypher_query(query_file_string)
+        query = f.read()
+        db.cypher_query(query)
     print("done!")
